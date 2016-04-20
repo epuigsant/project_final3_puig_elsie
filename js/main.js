b8d7347d0af3989fa52557c075519a05ba7b17a1
Elsie Puig <epuigsant@gmail.com>
2016-04-20 14:34:09 -0700

    clean up code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
+// JQuery Plugin - Mean Menu
+// Turns navigation into responsive hamburger menu
+
 $( document ).ready( function( ) {
     $( 'header nav' ).meanmenu();
 } );
 
+//JQuery Plugin - Text Rotator
+// Animates or rotates text
+
 $( '.rotate' ).textrotator( {
   animation: 'dissolve',
   speed: 2000
-} );
-
-$( document ).ready( function() {
-  $( '.bxslider' ).bxSlider();
 } );
